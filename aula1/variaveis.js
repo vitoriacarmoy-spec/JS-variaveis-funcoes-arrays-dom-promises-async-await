f3ee28a85e5188baafeb4d325b117488f686410d
vitoriacarmoy-spec <vitoria.carmoy@gmail.com>
2026-06-09 00:13:47 -0300

    aula1variavel

---
+++ aula1/variaveis.js
@@ -0,0 +1,65 @@
+/*
+const
+(nao pode mudar o valor inicial da constante)
+let(pode declarar qq tipo de valor e mostrar em paz)
+ex(let mamao= 40 mas const mamao = 40 nao, ja q ja e letra)
+var
+*/
+
+const mamao= "aaaa"
+const number = 20
+
+console.log(mamao)
+console.log(number)
+
+/*tipos de dados
+string,texto, aspas duplas ou simples ou crase(texto grande,variaveis ${numero}  )
+numbers
+boolean, true or false
+object=
+const usuario={
+nome:'rodolfo'     <--- tudo isso
+idade:20,
+casado:true
+}
+ null(ta vazio) & undefined(n tem)
+array - matriz (um monte de informaçao com campos iguais)
+const meuarray= [
+
+{
+nome:'rodolfo',
+idade:20,
+casado:false,
+mulhe:null},
+
+{
+nome:'rodrigo',
+idade:40,
+casado:true,
+mulhe:josi
+}
+
+
+]
+*/ 
+const texto = "gogogo"
+ 
+
+console.log(texto)
+
+const usuario={
+nome:'rodolfo',
+idade:20,
+casado:false,
+mulhe:null
+}
+const usuario2={
+nome:'rodrigo',
+idade:40,
+casado:true,
+mulhe:josi
+}
+
+console.log(usuario.mamao)
+
+console.log(usuario[5])
