02b0630bf6c81f32ced038a361eee9c331de6bd1
vitoriacarmoy-spec <vitoria.carmoy@gmail.com>
2026-06-18 22:23:13 -0300

    correçao

--- anotacao.js
+++ anotacao.js
@@ -3,61 +3,45 @@ asincrono mostra na hora
 pode por uma funçao assincrona real no objeto de promessa 
 async assincrono
 await espera completar*/
-syncFunc()
-console.log('Hello!')
-
-asyncFunc() 
-console.log('Hello!')
-
-function getWeather(){
-    return new Promise(function(resolve,reject))
-setTimeout(() => {
-resolve('Sunny')
-    },100)
+function getWeather() {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            resolve('Sunny')
+        }, 100)
+    })
 }
 
-/** antiquado 
-let promise = getWeather()
-promise.then(function(data)){
-    console.log(data)
-},function(data){
-    console.log(`First param ${data}`)
-}
-function(data){
-    console.log(`Second param ${data}`)
-}
-*/
-function getWeatherIcon(weather){
- return new Promise(function(resolve,reject))
-setTimeout(() => {
-    switch(weather){
-        case 'Sunny':
-            resolve ('a')
-            break
-            case 'Cloudy': 
-            resolve('e')
-            break
-            case 'Rainy': 
-            resolve('i')
-            break
-            default:
-                reject('NO ICON FOUND')
-    }
-resolve('Sunny')
-    },100)
+function getWeatherIcon(weather) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(() => {
+            switch (weather) {
+                case 'Sunny':
+                    resolve('☀️')
+                    break
+
+                case 'Cloudy':
+                    resolve('☁️')
+                    break
+
+                case 'Rainy':
+                    resolve('🌧️')
+                    break
+
+                default:
+                    reject('NO ICON FOUND')
+            }
+        }, 100)
+    })
 }
 
-
-function onSucess(){
-    
-    console.log(`Sucess param ${data}`)
+function onSucess(data) {
+    console.log(`Success param ${data}`)
 }
 
-function onError(error){
-    
+function onError(error) {
     console.log(`Error param ${error}`)
 }
 
 getWeather()
-.then(getWeatherIcon)
-.then(onSucess,onError)
+    .then(getWeatherIcon)
+    .then(onSucess, onError)
